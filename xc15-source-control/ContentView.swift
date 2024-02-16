12ebc8a913ad958a49efc4af80bee759dc10a687
Rachel Page <rachel.chwazik@verizonwireless.com>
2024-02-16 14:20:00 -0500

    Initial Commit

---
+++ xc15-source-control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xc15-source-control
+//
+//  Created by Rachel Page on 2/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xc15-source-control/xc15_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc15_source_controlApp.swift
+//  xc15-source-control
+//
+//  Created by Rachel Page on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct xc15_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
